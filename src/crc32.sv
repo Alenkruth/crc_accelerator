c71af343cea721ddad06f7292a03767336ef0393
Alenkruth Krishnan Murali <alenkruth@gmail.com>
2023-04-20 13:59:31 -0400

    update crc for the AXI IP

--- src/crc32.sv
+++ src/crc32.sv
@@ -12,40 +12,46 @@
 module crc32(
     input clk_i,
     input rst_i,
-    input compute_i,
+    input wire [31:0] compute_i,
     input wire [31:0] message_i,
-    //input [31:0] polynomial,
-    output reg [31:0] message_o
+    input wire [31:0] polynomial_i,
+    output wire [31:0] message_o
     );
     
     reg [31:0] polynomial = 32'h04C11DB7;  // edb88320;
+    // assign message_o = message_i ^ polynomial;
     reg [63:0] padded_message_i = {message_i, 32'h0};
     reg [63:0] padded_polynomial = {polynomial, 32'h0};
     reg [5:0] count = 6'd63;
+    reg [31:0] crc_out = 'h0;
     
-    always_ff @(posedge clk_i or negedge rst_i) begin
+    wire compute = compute_i[0];
+    
+    assign message_o = crc_out;
+    
+    always @(posedge clk_i or negedge rst_i) begin
         if (!rst_i) begin
-            message_o <= 32'h0;
+            crc_out <= 32'h0;
             // reset is required before computation can start. Else the old values of message will be read
             padded_polynomial <= {polynomial, 32'h0};
             padded_message_i <= {message_i, 32'h0};
             count <= 6'd63;
         end
         else begin
-            if (compute_i) begin
+            if (compute) begin
                 if (padded_message_i[count]) begin
                     padded_message_i <= padded_message_i ^ padded_polynomial;
                 end
                 padded_polynomial <= padded_polynomial >> 1;
                 count <= count - 1;
                 if (count == 6'd32) begin
-                    message_o <= padded_message_i[31:0];
+                    crc_out <= padded_message_i[31:0];
                 end
-                else begin
+                /*else begin
                     message_o <= 32'h0;
-                end
+                end*/
             end
-            else message_o <= 32'h0;
+            /*else message_o <= 32'h0;*/
         end
     end
     /*generate
